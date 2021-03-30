4525866bcdc95b0e38c56ecd4c514f36fa5c0ff1
Utkarsh Patel <imutkarshpatel@gmail.com>
2021-03-31 00:21:25 +0530

    Update make_training_examples.py

--- make_training_examples.py
+++ make_training_examples.py
@@ -1,6 +1,6 @@
 # Author: Utkarsh Patel
 #
-# preparing labeled.tsv and test.tsv
+# preparing labeled.tsv and unlabeled.tsv
 
 from Preprocess import Preprocess
 import pandas as pd
@@ -12,9 +12,9 @@ from tqdm import tqdm
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("--count", default=None, type=int, required=True, help="Number of examples to put in training set")
+  parser.add_argument("--ratio", default=None, type=float, required=True, help="ratio of training set to be used as labeled examples")
   parser.add_argument("--indir", default=None, type=str, required=True, help="directory of json files containing comments")
-  parser.add_argument("--outdir", default=None, type=str, required=True, help="directory to store labeled.tsv and test.tsv files")
+  parser.add_argument("--outdir", default=None, type=str, required=True, help="directory to store labeled.tsv and unlabeled.tsv files")
   args = parser.parse_args()
   pr = Preprocess()
 
@@ -22,50 +22,51 @@ def main():
   comments = list(reader['body'])
   violated_rule = list(reader['violated_rule'])
 
+  labeled_comments = comments[: int(args.ratio * len(comments))]
+  unlabeled_comments = comments[int(args.ratio * len(comments)):]
+
+  labeled_rule = violated_rule[: int(args.ratio * len(violated_rule))]
+  unlabeled_rule = violated_rule[int(args.ratio * len(violated_rule)):]
+
   writer_addr = os.path.join(args.outdir, 'labeled.tsv')
   writer = open(writer_addr, 'w')
   writer.write('label comments\n')
 
-  train_ah = 0
-  train_none = 0
+  labeled_ah = 0
+  labeled_none = 0
 
-  for i in tqdm(range(args.count), unit=" comments", desc="comments processed"):
+  print('Preparing labeled.tsv...')
+
+  for i in tqdm(range(labeled_comments), unit=" comments", desc="comments processed"):
     label = 'NONE'
-    train_none += 1
-    if violated_rule[i] == 2:
+    labeled_none += 1
+    if labeled_rule[i] == 2:
       label = 'AH'
-      train_ah += 1
-      train_none -= 1
-    cur = pr.preprocess(comments[i])
+      labeled_ah += 1
+      labeled_none -= 1
+    cur = pr.preprocess(labeled_comments[i])
     cur = ' '.join(cur)
     cur = label + ' ' + cur + '\n'
     writer.write(cur)
   
   writer.close()
 
-  writer_addr = os.path.join(args.outdir, 'test.tsv')
+  writer_addr = os.path.join(args.outdir, 'unlabeled.tsv')
   writer = open(writer_addr, 'w')
   writer.write('label comments\n')
 
-  test_ah = 0
-  test_none = 0
+  print('Preparing unlabeled.tsv...')
 
-  for i in tqdm(range(args.count, len(comments)), unit=" comments", desc="comments processed"):
-    label = 'NONE'
-    test_none += 1
-    if violated_rule[i] == 2:
-      label = 'AH'
-      test_ah += 1
-      test_none -= 1
-    cur = pr.preprocess(comments[i])
+  for i in tqdm(range(unlabeled_comments), unit=" comments", desc="comments processed"):
+    label = 'UNK'
+    cur = pr.preprocess(unlabeled_comments[i])
     cur = ' '.join(cur)
     cur = label + ' ' + cur + '\n'
     writer.write(cur)
   
   writer.close()
 
-  print(f'Training set: AH: {train_ah} - NONE: {train_none}')
-  print(f'Test set: AH: {test_ah} - NONE: {test_none}')
+  print(f'Training set: AH: {labeled_ah} - NONE: {labeled_none}')
   
 if __name__ == '__main__':
   main()
