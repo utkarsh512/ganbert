6eb86367ee99fdac7ead4363160e7fdcb809fcf1
Utkarsh Patel <imutkarshpatel@gmail.com>
2021-03-31 00:24:19 +0530

    Update make_test_examples.py

--- make_test_examples.py
+++ make_test_examples.py
@@ -1,6 +1,6 @@
 # Author: Utkarsh Patel
 #
-# preparing labeled.tsv and test.tsv
+# preparing test.tsv
 
 from Preprocess import Preprocess
 import pandas as pd
@@ -12,36 +12,14 @@ from tqdm import tqdm
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("--count", default=None, type=int, required=True, help="Number of examples to put in training set")
   parser.add_argument("--indir", default=None, type=str, required=True, help="directory of json files containing comments")
-  parser.add_argument("--outdir", default=None, type=str, required=True, help="directory to store labeled.tsv and test.tsv files")
+  parser.add_argument("--outdir", default=None, type=str, required=True, help="directory to store test.tsv file")
   args = parser.parse_args()
   pr = Preprocess()
 
   reader = pd.read_json(args.indir, lines=True, compression=None)
   comments = list(reader['body'])
   violated_rule = list(reader['violated_rule'])
-
-  writer_addr = os.path.join(args.outdir, 'labeled.tsv')
-  writer = open(writer_addr, 'w')
-  writer.write('label comments\n')
-
-  train_ah = 0
-  train_none = 0
-
-  for i in tqdm(range(args.count), unit=" comments", desc="comments processed"):
-    label = 'NONE'
-    train_none += 1
-    if violated_rule[i] == 2:
-      label = 'AH'
-      train_ah += 1
-      train_none -= 1
-    cur = pr.preprocess(comments[i])
-    cur = ' '.join(cur)
-    cur = label + ' ' + cur + '\n'
-    writer.write(cur)
-  
-  writer.close()
 
   writer_addr = os.path.join(args.outdir, 'test.tsv')
   writer = open(writer_addr, 'w')
@@ -50,7 +28,7 @@ def main():
   test_ah = 0
   test_none = 0
 
-  for i in tqdm(range(args.count, len(comments)), unit=" comments", desc="comments processed"):
+  for i in tqdm(range(len(comments)), unit=" comments", desc="comments processed"):
     label = 'NONE'
     test_none += 1
     if violated_rule[i] == 2:
@@ -64,7 +42,6 @@ def main():
   
   writer.close()
 
-  print(f'Training set: AH: {train_ah} - NONE: {train_none}')
   print(f'Test set: AH: {test_ah} - NONE: {test_none}')
   
 if __name__ == '__main__':
